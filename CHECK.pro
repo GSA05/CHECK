c02e2339922abfa692bed2383870a9cc628483b5
Sergey Goremykin <GSA05@mail.ru>
2015-03-10 17:56:09 +0300

    Пример 'Калькулятор обратной польской нотации'

--- CHECK.pro
+++ CHECK.pro
@@ -15,4 +15,11 @@ CONFIG   -= app_bundle
 TEMPLATE = app
 
 
-SOURCES += main.cpp
+SOURCES += main.cpp \
+    rpcalc.cpp
+
+OTHER_FILES += \
+    rpcalc.y
+
+HEADERS += \
+    rpcalc.hpp
--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <QCoreApplication>
+#include "rpcalc.hpp"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    yyparse();
 
     return a.exec();
 }
